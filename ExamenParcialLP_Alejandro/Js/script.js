9cb1d1940e774689613420fd6f89564971dd2f1e
nemidebyalep <nehemiasalejandro@upeu.edu.pe>
2019-04-14 11:18:02 -0500

    Examen Parcial
    Codigo Fuente

---
+++ ExamenParcialLP_Alejandro/Js/script.js
@@ -0,0 +1,27 @@
+function inicio(){
+    document.getElementById("inicio").style.display= "block";
+    document.getElementById("ventas").style.display= "none";
+    document.getElementById("clientes").style.display= "none";
+    document.getElementById("productos").style.display= "none";
+}
+function clientes(){
+
+    document.getElementById("inicio").style.display= "none";
+    document.getElementById("ventas").style.display= "none";
+    document.getElementById("clientes").style.display= "block";
+    document.getElementById("productos").style.display= "none";
+}
+function ventas(){
+    document.getElementById("inicio").style.display= "none";
+    document.getElementById("ventas").style.display= "block";
+    document.getElementById("clientes").style.display= "none";
+    document.getElementById("productos").style.display= "none";
+}
+function productos(){
+    document.getElementById("inicio").style.display= "none";
+    document.getElementById("ventas").style.display= "none";
+    document.getElementById("clientes").style.display= "none";
+    document.getElementById("productos").style.display= "block";
+    
+}
+    
